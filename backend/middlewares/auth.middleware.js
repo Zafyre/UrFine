9b28ec5e44ec91da9baabe652a50502578e7d9e1
Utkarsh Gupta <utkarshgupta2504@gmail.com>
2022-05-12 23:00:07 +0530

    Modified auth middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,29 +1,23 @@
 const jwt = require("jsonwebtoken");
 
-module.exports =
-	(role = "Any") =>
-	(req, res, next) => {
-		// get token from header
-		const token = req.header("x-auth-token");
+module.exports = (req, res, next) => {
+	// get token from header
+	const token = req.header("x-auth-token");
 
-		if (!token) {
-			return res.status(401).json({ message: "No Token!" });
-		}
+	if (!token) {
+		return res.status(401).json({ message: "No Token!" });
+	}
 
-		// verify token
-		try {
-			// decode token
-			const decoded = jwt.verify(token, process.env.JWT_SECRET);
+	// verify token
+	try {
+		// decode token
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-			// set req user = decoded user
-			req.user = decoded;
+		// set req user = decoded user
+		req.user = decoded;
 
-			if (role !== "Any" && req.user.role != role) {
-				return res.status(403).json({ message: "Invalid Role" });
-			}
-
-			next(); // callback, important , o.w. we'll get stuck here only
-		} catch (err) {
-			res.status(401).json({ message: "Invalid / Expired Token!" });
-		}
-	};
+		next(); // callback, important , o.w. we'll get stuck here only
+	} catch (err) {
+		res.status(401).json({ message: "Invalid / Expired Token!" });
+	}
+};
