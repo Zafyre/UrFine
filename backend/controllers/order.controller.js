1e68e43a5cf82385475de7ea16f656c114121001
Utkarsh Gupta <utkarshgupta2504@gmail.com>
2022-05-13 00:13:13 +0530

    Order controller

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,3 +1,31 @@
-module.exports.create = async (req, res) => {};
+const Order = require("../models/order.model");
+const appLogger = require("../utils/appLogger");
 
-module.exports.getUserOrders = async (req, res) => {};
+module.exports.create = async (req, res) => {
+	try {
+		const newOrder = new Order({
+			products: req.body.products,
+			user: req.user._id,
+		});
+
+		const order = newOrder.save();
+
+		res.status(201).json({ order });
+	} catch (err) {
+		appLogger(err);
+		res.status(500).json({ message: "Something went wrong", err });
+	}
+};
+
+module.exports.getUserOrders = async (req, res) => {
+	try {
+		const orders = await Order.find({ user: req.user._id })
+			.populate("products")
+			.exec();
+
+		res.status(200).json({ orders });
+	} catch (err) {
+		appLogger(err);
+		res.status(500).json({ message: "Something went wrong", err });
+	}
+};
--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -14,3 +14,6 @@ const orderSchema = mongoose.Schema({
 		},
 	],
 });
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
