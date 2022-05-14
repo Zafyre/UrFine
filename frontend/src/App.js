efbe3dec7e0947074991065f0ad3337ff3a92ad1
Zafyre <aaryanpratap11@gmail.com>
2022-05-14 13:46:46 +0530

    Added forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import Details from "./components/Details";
 import Error from "./components/Error";
 import Cart from "./components/Cart";
 import Modal from "./components/Modal";
+import NewProduct from "./components/newProduct/NewProduct";
+import NewUser from "./components/newUser/NewUser";
 
 function App() {
 	return (
@@ -16,6 +18,8 @@ function App() {
 				<Route exact path="/" component={Home} />
 				<Route exact path="/products" component={ProductList} />
 				<Route exact path="/details" component={Details} />
+				<Route exact path="/newproduct" component={NewProduct} />
+				<Route exact path="/newuser" component={NewUser} />
 				<Route path="/cart" component={Cart} />
 				<Route component={Error} />
 			</Switch>
