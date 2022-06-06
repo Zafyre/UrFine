55407a9b90dd0664a3acdcbf280c10fd02b147a6
Utkarsh Gupta <utkarshgupta2504@gmail.com>
2022-06-06 18:36:45 +0530

    Rerouted orders from payment

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -11,207 +11,170 @@ import DetailButton from "./DetailButton";
 import StripeCheckout from "react-stripe-checkout";
 
 const Cart = (props) => {
-  const value = useContext(product);
-  const orderValue = useContext(order);
+	const value = useContext(product);
+	const orderValue = useContext(order);
 
-  const history = useHistory();
+	const history = useHistory();
 
-  const cartItems = value.cart.map((item) => {
-    return (
-      <CartList
-        key={item._id}
-        id={item._id}
-        img={item.image}
-        title={item.name}
-        price={item.price}
-        count={item.count}
-      />
-    );
-  });
+	const cartItems = value.cart.map((item) => {
+		return (
+			<CartList
+				key={item._id}
+				id={item._id}
+				img={item.image}
+				title={item.name}
+				price={item.price}
+				count={item.count}
+			/>
+		);
+	});
 
-  const handleOrder = async (e) => {
-    e.preventDefault();
+	const Button = styled.button`
+		width: 100%;
+		padding: 10px;
+		background-color: black;
+		color: white;
+		font-weight: 600;
+	`;
 
-    try {
-      const response = await axios.post(
-        process.env.REACT_APP_API_URI + "/api/orders",
-        {
-          products: value.cart.map((item) => {
-            return { product: item._id, quantity: item.count };
-          }),
-          totalPrice: value.totalAmt,
-        },
-        {
-          headers: {
-            "x-auth-token": auth.getToken(),
-          },
-        }
-      );
+	if (value.cart.length === 0) {
+		return (
+			<div className="container">
+				<div className="main-heading-container">
+					<h1 className="main-heading">Your Cart</h1>
+				</div>
+				<h2>Your cart is empty... </h2>
+			</div>
+		);
+	} else {
+		if (window.innerWidth <= 450) {
+			return (
+				<div className="container cartContainer">
+					<div className="main-heading-container">
+						<h1 className="main-heading">Your Cart</h1>
+					</div>
 
-      console.log(response.data);
+					<div
+						className="row cartHeaderRow"
+						style={{ textAlign: "center", marginBottom: "15px" }}
+					>
+						<div className="col-5 product-name">
+							<b>Product Name</b>
+						</div>
+						<div className="col-3 product-quantity">
+							<b>Quantity</b>
+						</div>
+						<div className="col-2 product-remove">
+							<b>Remove</b>
+						</div>
+						<div className="col-2 product-amt">
+							<b>Item Total</b>
+						</div>
+					</div>
 
-      showToast("Order Cnfirmed", "success");
+					{cartItems}
 
-      value.clearCart();
+					<div
+						className="row"
+						style={{
+							textAlign: "right",
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<div className="col-12" style={{ paddingRight: "5%" }}>
+							<div
+								onClick={() => value.clearCart()}
+								style={{ display: "inline-block" }}
+							>
+								<DetailButton
+									btnTxt="Clear Cart"
+									btnClass="btn-outline-warning detailBtn"
+								/>
+							</div>
+							<h4 style={{ display: "inline-block", margin: "auto 10px" }}>
+								<b>Total: </b> <i className="fas fa-rupee-sign"></i>
+								{value.totalAmt}
+							</h4>
+						</div>
+					</div>
+					<br />
+				</div>
+			);
+		} else {
+			return (
+				<div className="container">
+					<div className="main-heading-container">
+						<h1 className="main-heading">Your Cart</h1>
+					</div>
 
-      orderValue.addOrder(response.data.order);
+					<div
+						className="row"
+						style={{ textAlign: "center", marginBottom: "15px" }}
+					>
+						<div className="col-2 product-img">
+							<b>Product</b>
+						</div>
+						<div className="col-3 product-name">
+							<b>Product Name</b>
+						</div>
+						<div className="col-2 product-price">
+							<b>Price</b>
+						</div>
+						<div className="col-2 product-quantity">
+							<b>Quantity</b>
+						</div>
+						<div className="col-1 product-remove">
+							<b>Remove</b>
+						</div>
+						<div className="col-2 product-amt">
+							<b>Item Total</b>
+						</div>
+					</div>
 
-      history.replace("/products");
-    } catch (err) {
-      if (err instanceof AxiosError) {
-        showToast("Please Login To Place Order", "error");
-      } else {
-        console.log(err);
-      }
-    }
-  };
+					{cartItems}
 
-  const Button = styled.button`
-    width: 100%;
-    padding: 10px;
-    background-color: black;
-    color: white;
-    font-weight: 600;
-  `;
+					<br />
 
-  if (value.cart.length === 0) {
-    return (
-      <div className="container">
-        <div className="main-heading-container">
-          <h1 className="main-heading">Your Cart</h1>
-        </div>
-        <h2>Your cart is empty... </h2>
-      </div>
-    );
-  } else {
-    if (window.innerWidth <= 450) {
-      return (
-        <div className="container cartContainer">
-          <div className="main-heading-container">
-            <h1 className="main-heading">Your Cart</h1>
-          </div>
+					<div
+						className="row"
+						style={{
+							textAlign: "right",
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<div className="col-12" style={{ paddingRight: "5%" }}>
+							<div
+								onClick={() => value.clearCart()}
+								style={{ display: "inline-block" }}
+							>
+								<DetailButton
+									btnTxt="Clear Cart"
+									btnClass="btn-outline-warning detailBtn"
+								/>
+							</div>
 
-          <div
-            className="row cartHeaderRow"
-            style={{ textAlign: "center", marginBottom: "15px" }}
-          >
-            <div className="col-5 product-name">
-              <b>Product Name</b>
-            </div>
-            <div className="col-3 product-quantity">
-              <b>Quantity</b>
-            </div>
-            <div className="col-2 product-remove">
-              <b>Remove</b>
-            </div>
-            <div className="col-2 product-amt">
-              <b>Item Total</b>
-            </div>
-          </div>
+							<Link to="/stripecontainer">
+								<DetailButton
+									btnTxt="Confirm Order"
+									btnClass="btn-outline-success detailBtn"
+								/>
+							</Link>
 
-          {cartItems}
-
-          <div
-            className="row"
-            style={{
-              textAlign: "right",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <div className="col-12" style={{ paddingRight: "5%" }}>
-              <div
-                onClick={() => value.clearCart()}
-                style={{ display: "inline-block" }}
-              >
-                <DetailButton
-                  btnTxt="Clear Cart"
-                  btnClass="btn-outline-warning detailBtn"
-                />
-              </div>
-              <h4 style={{ display: "inline-block", margin: "auto 10px" }}>
-                <b>Total: </b> <i className="fas fa-rupee-sign"></i>
-                {value.totalAmt}
-              </h4>
-            </div>
-          </div>
-          <br />
-        </div>
-      );
-    } else {
-      return (
-        <div className="container">
-          <div className="main-heading-container">
-            <h1 className="main-heading">Your Cart</h1>
-          </div>
-
-          <div
-            className="row"
-            style={{ textAlign: "center", marginBottom: "15px" }}
-          >
-            <div className="col-2 product-img">
-              <b>Product</b>
-            </div>
-            <div className="col-3 product-name">
-              <b>Product Name</b>
-            </div>
-            <div className="col-2 product-price">
-              <b>Price</b>
-            </div>
-            <div className="col-2 product-quantity">
-              <b>Quantity</b>
-            </div>
-            <div className="col-1 product-remove">
-              <b>Remove</b>
-            </div>
-            <div className="col-2 product-amt">
-              <b>Item Total</b>
-            </div>
-          </div>
-
-          {cartItems}
-
-          <br />
-
-          <div
-            className="row"
-            style={{
-              textAlign: "right",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <div className="col-12" style={{ paddingRight: "5%" }}>
-              <div
-                onClick={() => value.clearCart()}
-                style={{ display: "inline-block" }}
-              >
-                <DetailButton
-                  btnTxt="Clear Cart"
-                  btnClass="btn-outline-warning detailBtn"
-                />
-              </div>
-
-              <Link to="/stripecontainer">
-                <DetailButton
-                  btnTxt="Confirm Order"
-                  btnClass="btn-outline-success detailBtn"
-                />
-              </Link>
-
-              <h4 style={{ display: "inline-block", margin: "auto 10px" }}>
-                <b>Total: </b> <i className="fas fa-rupee-sign"></i>
-                {value.totalAmt}
-              </h4>
-            </div>
-          </div>
-          <br />
-        </div>
-      );
-    }
-  }
+							<h4 style={{ display: "inline-block", margin: "auto 10px" }}>
+								<b>Total: </b> <i className="fas fa-rupee-sign"></i>
+								{value.totalAmt}
+							</h4>
+						</div>
+					</div>
+					<br />
+				</div>
+			);
+		}
+	}
 };
 
 export default Cart;
--- frontend/src/components/Paymentform.jsx
+++ frontend/src/components/Paymentform.jsx
@@ -1,75 +1,115 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import axios from "axios";
-import React, { useState } from "react";
+import axios, { AxiosError } from "axios";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { order } from "../providers/order.provider";
+import { product } from "../providers/product.provider";
+import auth from "../utils/auth";
+import { showToast } from "../utils/toasts";
 
 const CARD_OPTIONS = {
-  iconStyle: "solid",
-  style: {
-    base: {
-      iconColor: "#c4f0ff",
-      color: "#fff",
-      fontWeight: 500,
-      fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
-      fontSize: "16px",
-      fontSmoothing: "antialiased",
-      ":-webkit-autofill": { color: "#fce883" },
-      "::placeholder": { color: "#87bbfd" },
-    },
-    invalid: {
-      iconColor: "#ffc7ee",
-      color: "#ffc7ee",
-    },
-  },
+	iconStyle: "solid",
+	style: {
+		base: {
+			iconColor: "#c4f0ff",
+			color: "#fff",
+			fontWeight: 500,
+			fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+			fontSize: "16px",
+			fontSmoothing: "antialiased",
+			":-webkit-autofill": { color: "#fce883" },
+			"::placeholder": { color: "#87bbfd" },
+		},
+		invalid: {
+			iconColor: "#ffc7ee",
+			color: "#ffc7ee",
+		},
+	},
 };
 
 export default function PaymentForm() {
-  const [success, setSuccess] = useState(false);
-  const stripe = useStripe();
-  const elements = useElements();
+	const [success, setSuccess] = useState(false);
+	const stripe = useStripe();
+	const elements = useElements();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement),
-    });
+	const value = useContext(product);
+	const orderValue = useContext(order);
 
-    if (!error) {
-      try {
-        const { id } = paymentMethod;
-        const response = await axios.post("http://localhost:4000/api/payment", {
-          amount: 1000,
-          id,
-        });
+	const history = useHistory();
 
-        if (response.data.success) {
-          console.log("Successful payment");
-          setSuccess(true);
-        }
-      } catch (error) {
-        console.log("Error", error);
-      }
-    } else {
-      console.log(error.message);
-    }
-  };
+	const handleOrder = async (e) => {
+		e.preventDefault();
 
-  return (
-    <>
-      {!success ? (
-        <form onSubmit={handleSubmit}>
-          <fieldset className="FormGroup">
-            <div className="FormRow">
-              <CardElement options={CARD_OPTIONS} />
-            </div>
-          </fieldset>
-          <button>Pay</button>
-        </form>
-      ) : (
-        <div>
-          <h2>Bought</h2>
-        </div>
-      )}
-    </>
-  );
+		try {
+			const response = await axios.post(
+				process.env.REACT_APP_API_URI + "/api/orders",
+				{
+					products: value.cart.map((item) => {
+						return { product: item._id, quantity: item.count };
+					}),
+					totalPrice: value.totalAmt,
+				},
+				{
+					headers: {
+						"x-auth-token": auth.getToken(),
+					},
+				}
+			);
+
+			console.log(response.data);
+
+			showToast("Order Cnfirmed", "success");
+
+			value.clearCart();
+			orderValue.addOrder(response.data.order);
+
+			history.replace("/products");
+		} catch (err) {
+			if (err instanceof AxiosError) {
+				showToast("Please Login To Place Order", "error");
+			} else {
+				console.log(err);
+			}
+		}
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const { error, paymentMethod } = await stripe.createPaymentMethod({
+			type: "card",
+			card: elements.getElement(CardElement),
+		});
+
+		if (!error) {
+			try {
+				const { id } = paymentMethod;
+				const response = await axios.post("http://localhost:4000/api/payment", {
+					amount: value.totalAmt * 100,
+					id,
+				});
+
+				if (response.data.success) {
+					console.log("Successful payment");
+					handleOrder(e);
+				}
+			} catch (error) {
+				console.log("Error", error);
+			}
+		} else {
+			console.log(error.message);
+		}
+	};
+
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<fieldset className="FormGroup">
+					<div className="FormRow">
+						<CardElement options={CARD_OPTIONS} />
+					</div>
+				</fieldset>
+				<button>Pay</button>
+			</form>
+		</>
+	);
 }
